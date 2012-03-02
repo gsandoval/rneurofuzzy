71262b4eeae3e4e88afcbaa2f9fa744ab8b1c170
Guillermo Sandoval <gsandoval@darchitect.org>
2012-03-01 18:05:05 -0600

    BP once more

--- backpropagation_network.rb
+++ backpropagation_network.rb
@@ -1,18 +1,30 @@
 
 class Neuron
-	attr_accessor :output, :weights, :bias
-	
+	attr_accessor :output
+
 	def initialize(next_layer_size)
-		output = Array.new(next_layer_size)
-		weights, bias = [], []
+		@output = Array.new next_layer_size 
+		@weights, @bias = [], []
 		(1..next_layer_size).each do
-			weights.push rand * 2 - 1
-			bias.push rand * 2 - 1
+			@weights.push rand * 2 - 1
+			@bias.push rand * 2 - 1
 		end
 	end
 	
-	def fire
-		
+	def activation_function(t)
+		1.0 / (1 + Math::exp(-t))
+	end
+	
+	def fire(n)
+		@output[n] = @forced ? @forced[n] : activation_function(@weights[n] * @input + @bias[n])
+	end
+	
+	def input(v)
+		@input = v.reduce :+
+	end
+	
+	def force_output(forced_output)
+		@forced = forced_output
 	end
 end
 
@@ -26,16 +38,20 @@ class BackPropagationNetwork
 		end
 	end
 	
+	def train(ps)
+		
+	end
+	
 	def classify(pattern)
-		@layers[0].zip(pattern).each {|p| p[0].output = p[1]}
+		@layers[0].zip(pattern).each {|p| p[0].force_output [p[1]] * @layers[1].size}
 		for i in (1..@layers.size-1)
 			prev, curr = @layers[i-1], @layers[i]
 			for j in (0..curr.size-1)
-				input = []
+				v = []
 				for k in (0..prev.size-1)
-					input.push(prev[k].output[j] * prev[k].weights[j])
+					v.push(prev[k].fire(j))
 				end
-				curr[j].fire input
+				curr[j].input v
 			end
 		end
 	end
--- bp_test.rb
+++ bp_test.rb
@@ -2,4 +2,5 @@
 
 require 'backpropagation_network'
 
-n = BackPropagationNetwork.new(0.2, 4, 1, 4, 2)
+n = BackPropagationNetwork.new(0.2, 3, 1, 4, 2)
+n.classify [0, 1, 0]
