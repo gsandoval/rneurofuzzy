cdd43e95958e9d272ad474cde86e5e12a35eb9a1
Guillermo Sandoval <gsandoval@darchitect.org>
2012-03-08 10:39:50 -0600

    Back prop is alive.

--- backpropagation_network.rb
+++ backpropagation_network.rb
@@ -1,28 +1,37 @@
 
 class BackPropagationNetwork
 	attr_accessor :max_error, :max_epochs
-	attr_reader :learning_rate
+	attr_reader :learning_rate, :momentum
 	def initialize(input_size, output_size, *args)
-		@layers = []
-		sizes = [input_size] + args + [output_size] + [0]
-		for i in (0..sizes.size-2)
-			curr_layer_size, next_layer_size = sizes[i], sizes[i+1]
-			if i == 0
-				neurons = (1..curr_layer_size).map {InputNeuron.new(next_layer_size)}
-				neurons.push BiasNeuron.new(next_layer_size)
-			elsif i == sizes.size - 2
-				neurons = (1..curr_layer_size).map {OutputNeuron.new(next_layer_size)}
-			else
-				neurons = (1..curr_layer_size).map {Neuron.new(next_layer_size)}
-				neurons.push BiasNeuron.new(next_layer_size)
-			end
-			@layers.push(neurons)
+		@input_layer = (0..input_size).map {|i| Neuron.new(1)}
+		@input_layer.last.raw_input = 1 # Bias
+		prev_size = input_size
+		@hidden_layers = []
+		for size in args
+			@hidden_layers.push((0..size).map {|i| Neuron.new(prev_size + 1)})
+			@hidden_layers.each {|lyr| lyr.last.raw_input = 1} # Bias
+			prev_size = size
 		end
+		@output_layer = (1..output_size).map {|i| Neuron.new(prev_size + 1)}
+	end
+	
+	def trained_weights=(tw)
+		nn = []
+		@hidden_layers.each {|layer| layer.each {|neuron| nn.push neuron}}
+		@output_layer.each {|neuron| nn.push neuron}
+		nn.zip(tw).each {|n, w| n.weights = w}
 	end
 	
 	def learning_rate=(lr)
 		@learning_rate = lr
-		@layers.each {|layer| layer.each {|neuron| neuron.learning_rate = lr}}
+		@hidden_layers.each {|layer| layer.each {|neuron| neuron.learning_rate = lr}}
+		@output_layer.each {|neuron| neuron.learning_rate = lr}
+	end
+	
+	def momentum=(m)
+		@momentum = m
+		@hidden_layers.each {|layer| layer.each {|neuron| neuron.momentum = m}}
+		@output_layer.each {|neuron| neuron.momentum = m}
 	end
 	
 	def train(ps, es)
@@ -31,113 +40,102 @@ class BackPropagationNetwork
 			epoch_error = 0
 			ps.zip(es).each do |p, e|
 				y = classify p
-				d = [e, y].transpose.map {|x| x.reduce :-}
-				backpropagate d
-				epoch_error += d.map {|err| err**2}.reduce :+
-				puts "============================="
+				e = e.map {|x| x == 0 ? 0.00000000001 : (x == 1 ? 0.999999999999 : x)}
+				err = [y, e].transpose.map {|x| x.reduce :-}
+				backpropagate err
+				epoch_error += err.map {|e| e**2}.reduce :+
 			end
 			break if epoch_error.nan?
-			epoch_error /= es.size
-			puts "epoch: #{epoch}, epoch error: #{epoch_error}"
+			#epoch_error = Math::sqrt(epoch_error / es.size)
+			epoch_error /= 2.0
+			
+			#if epoch % 1000 == 0
+			#	puts "epoch: #{epoch}, epoch error: #{epoch_error}"
+			#	ps.zip(es).each do |p, e|
+			#		y = classify p
+			#		puts "#{p.inspect} -> #{y.inspect}"
+			#	end
+			#end
+			puts "epoch #{epoch}: #{epoch_error}"
+			
 			break if epoch_error < @max_error
 		end
-		#puts epoch_error
+		puts "Final epoch error: #{epoch_error}"
+		trained_weights = []
+		@hidden_layers.each {|layer| layer.each {|neuron| trained_weights.push neuron.weights}}
+		@output_layer.each {|neuron| trained_weights.push neuron.weights}
+		trained_weights
 	end
 	
 	def classify(pattern)
-		pattern.zip(@layers[0]).each {|p| p[1].input = p[0]}
-		for i in (1..@layers.size-1)
-			prev, curr = @layers[i-1], @layers[i]
-			curr_layer_input = @layers.last == curr ? curr.size-1 : curr.size-2 # Bias extra neuron
-			for j in (0..curr_layer_input)
-				curr[j].input = (0..prev.size-1).map {|k| prev[k].fire(j)}
-			end
+		pattern.zip(@input_layer).each {|p| p[1].raw_input = p[0]}
+		inp = @input_layer.map {|n| n.output}
+		@hidden_layers.each do |layer|
+			layer[0..layer.size-2].each {|n| n.input = inp}
+			inp = layer.map {|n| n.output}
 		end
-		@layers.last.map {|n| n.fire(0)}
+		@output_layer.each {|n| n.input = inp}
+		@output_layer.map {|n| n.output}
 	end
 	
 	private
 	
-	def backpropagate(d)
-		d.zip(@layers.last).each {|od, neuron| neuron.delta = od}
-		(@layers.size-1).downto(1) do |i|
-			nxt, curr = @layers[i-1], @layers[i]
-			curr_layer_input = @layers.last == curr ? curr.size-1 : curr.size-2 # Bias extra neuron
-			for j in (0..nxt.size-1)
-				ds = (0..curr_layer_input).map {|k| curr[k].delta}
-				nxt[j].delta = ds
+	def backpropagate(err)
+		err.zip(@output_layer).each {|e, neuron| neuron.delta = [e]}
+		prev_layer = @output_layer
+		@hidden_layers.reverse.each do |curr|
+			for i in (0..curr.size-1)
+				curr[i].delta = (0..prev_layer.size-1).map {|j| prev_layer[j].delta(i)}
 			end
+			prev_layer = curr
 		end
+		prev_layer = @input_layer
+		@hidden_layers.each do |layer|
+			layer.each {|neuron| neuron.adjust_weights prev_layer.map{|n| n.output}}
+			prev_layer = layer
+		end
+		@output_layer.each {|neuron| neuron.adjust_weights prev_layer.map{|n| n.output}}
 	end
 end
 
 class Neuron
-	attr_accessor :output, :learning_rate
-	attr_reader :delta
+	attr_accessor :learning_rate, :momentum, :raw_input, :weights
 
-	def initialize(next_layer_size)
-		@output = Array.new next_layer_size
-		@weights = (1..next_layer_size).map {|v| rand * 2 - 1}
-	end
-	
-	def fire(n)
-		@output[n] = activation_function(@weights[n] * @input)
+	def initialize(prev_layer_size)
+		@weights = (1..prev_layer_size).map {|v| rand * 2 - 1}
+		@last_weight_update = Array.new(prev_layer_size, 0)
 	end
 	
 	def input=(v)
-		@input = v.reduce :+
+		@raw_input = ([@weights, v].transpose.map {|x| x.reduce :*}).reduce :+
+	end
+	
+	def output
+		@output = activation_function(@raw_input)
 	end
 	
 	def delta=(d)
-		@delta = [d.take(@weights.size), @weights].transpose.map {|x| x.reduce :*}.reduce(:+)
-		puts "before #{@weights.inspect}"
-		@weights = @weights.zip(@output, d).map {|w, out, dlt| w + @learning_rate * dlt * activation_function_derivative(out) * @input}
-		puts "after  #{@weights.inspect}"
+		@delta = d.reduce :+
+	end
+	
+	def delta(n)
+		@delta * @weights[n]
+	end
+	
+	def adjust_weights(prev_layer_input)
+		@last_weight_update = @last_weight_update.zip(prev_layer_input).map {|lu, inp| -@learning_rate * @delta * activation_function_derivative(@raw_input) * inp + @momentum * lu}
+		@weights = @weights.zip(@last_weight_update).map {|w, lu| w + lu}
 	end
 	
 	private
-	
-	#def activation_function(t)
-	#	t > 0 ? 1.0 : 0.0
-	#end
-	
-	#def activation_function_derivative(t)
-	#	1
-	#end
 	
 	def activation_function(t)
 		1.0 / (1 + Math::exp(-t))
 	end
 	
 	def activation_function_derivative(t)
-		t * (1 - t)
+		fx = 1.0 / (1 + Math::exp(-t))
+		fx * (1 - fx)
 	end
 end
 
-class InputNeuron < Neuron
-	def input=(v)
-		@input = v
-	end
-end
-
-class OutputNeuron < Neuron
-	def fire(n)
-		@output[n] = activation_function(@input)
-	end
-	
-	def delta=(d)
-		@delta = d
-	end
-end
-
-class BiasNeuron < Neuron
-	def initialize(next_layer_size)
-		@weights = (1..next_layer_size).map {|v| rand * 2 - 1}
-		@output = Array.new next_layer_size
-		@input = 1.0
-	end
-	
-	def fire(n)
-		@output[n] = @weights[n]
-	end
-end
--- bp_test.rb
+++ bp_test.rb
@@ -2,12 +2,28 @@
 
 require 'backpropagation_network'
 
-n = BackPropagationNetwork.new(2, 1, 3, 2)
-n.learning_rate = 0.5
+n = BackPropagationNetwork.new(2, 1, 3)
+n.learning_rate = 0.7
+n.momentum = 0.3
 n.max_error = 0.0001
-n.max_epochs = 1
+n.max_epochs = 50000
 
 patterns = [[0, 0], [1, 0], [0, 1], [1, 1]]
-expected = [[0], [1], [1], [0]]
-n.train patterns, expected
-#puts n.classify [0, 1, 0]
+expected = [[0.00001], [1], [1], [0.00001]]
+trained_weights = n.train patterns, expected
+
+puts
+puts trained_weights.inspect
+puts
+
+n.trained_weights = [[-23.0217773437391, -22.8139049001462, 32.6026950655199],
+[21.4243407311869, -32.9338898394372, 5.47305730031733],
+[-33.3855565563251, 22.9474149772918, 4.40321880916154],
+[-0.853068673429263, 0.955086312185503, -35.5191444793863],
+[-7.02187697749253, 10.0851640847795, 10.0165774181415, -6.34209667187675]]
+
+
+puts n.classify([0, 0]).inspect
+puts n.classify([0, 1]).inspect
+puts n.classify([1, 0]).inspect
+puts n.classify([1, 1]).inspect
